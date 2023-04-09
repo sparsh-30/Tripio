ec29e90ba312403516adaa3ef33eb8b032b8b106
Sparsh Gupta <gsparsh04@gmail.com>
2023-04-09 19:50:02 +0530

    Home page created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,26 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import './App.css';
+import Home from './components/pages/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
 
 function App() {
-
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' exact element={<Home />} />
+          <Route path='/services' element={<Services />} />
+          <Route path='/products' element={<Products />} />
+          <Route path='/sign-up' element={<SignUp />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/components/pages/Products.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import '../../App.css';
+
+export default function Products() {
+  return <div><h1 className='products'>PRODUCTS</h1></div>;
+}
